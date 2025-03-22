a6b3ff4e56690dc9aaa6314d6e42a899d3d968d4
sl1ppy <serzh.skryt.93@gmail.com>
2025-03-22 20:00:42 +1000

    test

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  // compatibilityDate: '2024-11-01',
+  // devtools: { enabled: true },
+  // ssr: true,
+  css: ['~/assets/styles/style.scss'],
+
+  ssr: false,
+  compatibilityDate: '2025-03-22',
+})
